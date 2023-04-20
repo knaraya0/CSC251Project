cc0b1422d0ef9f9ddf11c4bee22218536dfca468
Kabir Narayan <knaraya0@email.cpcc.edu>
2023-04-20 16:42:25 -0400

    Added toString method

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -1,3 +1,20 @@
 Public class PolicyHolder
 {
-   
+   private String policyFirstName;
+   private String policyLastName;
+   private double policyAge;
+   private double policyHeight;
+   private double policyWeight;
+   private String policySmokingStatus;
+
+   public String toString()
+      {
+         return String.format("policy holder's first name: " + policyFirstName + 
+                "\n policy holder's last name: " + policyLastName + 
+                "\n policy holder's age: " + policyAge +
+                "\npolicy holder's height: " + policyHeight + 
+                "policy holder's weight: " + policyWeight + 
+                "policy holder's smoking status: " + policySmokingStatus);
+                
+      }
+}
